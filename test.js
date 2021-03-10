b3ef70a7f6245f3ef0e5e29e6d0d1eeb1986e810
samod singh <samod0098@gmail.com>
2021-03-10 21:57:31 +0530

    createelement-createtextnode

---
+++ test.js
@@ -0,0 +1,12 @@
+
+
+var newElement = document.createElement("h2")
+
+var newText = document.createTextNode("this is just text");
+
+var newComment = document.createComment("this is comment");
+
+console.log(newElement);
+console.log(newText);
+console.log(newComment);
+
